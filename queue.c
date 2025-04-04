ccda35016100de052a9dba992dac3def3e4768c8
Aniketh Bhaskar Bandi <bandi4@eceprog6.ecn.purdue.edu>
2025-04-04 17:52:49 -0400

    deque

--- queue.c
+++ queue.c
@@ -2,14 +2,17 @@
 #include "tile_game.h"
 
 void enqueue(struct queue *q, struct game_state state) {
-    
+    uint64_t val = serialize(state);
+    insert_at_head(&(q->data), val);
+
 }
 
 struct game_state dequeue(struct queue *q) {
     
-    
-    
-    return (struct game_state){0};
+    uint64_t val = remove_from_tail(&(q->data));
+    struct game_state game1 = deserialize(val);
+
+    return game1;
 }
 
 int number_of_moves(struct game_state start) {
