0345d90765e0362e97b715eeb60f9037ef063b61
Aniketh Bhaskar Bandi <bandi4@eceprog6.ecn.purdue.edu>
2025-04-04 17:02:38 -0400

    linked

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,65 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *malNode = malloc(sizeof(struct list_node));
+  malNode->value = value;
+  malNode->next = NULL;
+  
+  return malNode;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+struct list_node *newHead = new_node(value);\
+newHead->next = list->head;
+list->head = newHead;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *currentNode = list->head; 
+  while (currentNode->next != NULL) {
+    currentNode = currentNode->next;
+  }
+  currentNode->next = new_node(value);
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  size_t retVal = list->head->value;
+  struct list_node *temp = list->head->next;
+  free(list->head);
+  list->head = temp;
 
-void free_list(struct linked_list list) {}
+  return retVal;
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+  struct list_node *current = list->head;
+  struct list_node *previous = NULL;
+  while (current->next != NULL)
+  {
+    previous = current;
+    current = current->next;
+  }
+  size_t retVal = current->value;
+  free(current);
+  previous->next = NULL;
+  
+  return retVal;
+}
+
+void free_list(struct linked_list list) {
+struct list_node *current = list.head;
+struct list_node *next = NULL;
+
+while (current != NULL)
+{
+  next = current->next;
+  free(current);
+  current = next;
+}
+
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- linked_list.h
+++ linked_list.h
@@ -13,6 +13,7 @@ struct linked_list {
   struct list_node *head;
 };
 
+
 void insert_at_head(struct linked_list *list, size_t value);
 void insert_at_tail(struct linked_list *list, size_t value);
 size_t remove_from_head(struct linked_list *list);
